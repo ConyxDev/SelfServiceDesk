0f9935c6f9e1f7e6b0dc5865e5a66b4ec7cfdd84
Conyx Dev <conyxdev@gmail.com>
2025-03-11 22:42:49 +0100

    feat(exo1) set loop for products still need to style the design

--- src/app/pages/order-page/order-page.component.ts
+++ src/app/pages/order-page/order-page.component.ts
@@ -1,32 +1,48 @@
 import { Component } from '@angular/core';
 import { API } from '../../services/API';
+import { NgFor, CommonModule } from '@angular/common';
 
 //interface to do
+interface Category {
+  title: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  recipes: any[];
+}
 
-
-
-
+interface Restaurant {
+  title: string;
+  description: string;
+  photo: string;
+  etaRange: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrl: './order-page.component.css'
+  styleUrl: './order-page.component.css',
+  imports: [NgFor, CommonModule]
 })
 export class OrderPageComponent {
-  recipes: any[] = []; //interface to do
-  restaurant: any = {};
-
+  categories: Category[] = []; //interface to do
+  restaurant: Restaurant = {} as Restaurant ;
+  products: any[] = [];
+  recipes: any[] = [];
 
   constructor() {
     this.getRecipes();
   }
 
   getRecipes() {
-    new API().getRecipes().then((data) => {
-      this.recipes = data.data;
+      new API().getRecipes().then((data) => {
+      this.categories = data.data;
       this.restaurant = data;
-      console.log(this.recipes);
-      console.log(this.restaurant);
+      this.products = data.data.recipes;
+      console.log('categories', this.categories);
+      console.log('restaurant', this.restaurant);
+      console.log('products', this.products);
     });
   }
 
