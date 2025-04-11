c2659cfb786aedb22bd5c40817099a22093d35a6
Conyx Dev <conyxdev@gmail.com>
2025-04-11 10:36:20 +0200

    feat(serviceWorker)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'self-service-desk',
+  webDir: 'www'
+};
+
+export default config;
